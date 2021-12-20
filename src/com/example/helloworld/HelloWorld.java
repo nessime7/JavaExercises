544cc85384cc1862a58182f6b4f87d5b5e0ef671
nessime7 <sara.przebinda@gmail.com>
2021-12-20 19:54:09 +0100

    HelloWorld file added

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.example.helloworld;
+
+public  class HelloWorld {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println("hello world " + i);
+        }
+
+
+    }
+}
